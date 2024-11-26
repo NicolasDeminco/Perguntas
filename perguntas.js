6bd96eef7479a5bcfdb374aabbe455341afe8edc
Nicolas Deminco <00001145919352sp@al.educacao.sp.gov.br>
2024-11-26 02:22:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'Quai as principais sub-partículas que constituem o átomo?',
+    'Prótons, elétrons e nêutrons.'
+)
+
+criaCartao(
+    'Química',
+    'Quantos elementos existem na tabela periódica?',
+    '118 elementos sendo 92 naturais e 26 artificiais'
+)
+
+criaCartao(
+    'Conhecimentos básicos',
+    'Quem é o aluno mais legal da escola Raquel de Castro?',
+    'Nicolas Deminco :)'
+)
+
+criaCartao(
+    'Conhecimentos básicos',
+    'Quem é a professora mais legal da escola Raquel de Castro?',
+    'Renata Rolim :)'
+)
